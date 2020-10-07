6f56e3e328ef0f80d1163e86a7127a818fe71479
Vladimir Lypak <junak.pub@gmail.com>
2020-10-07 13:08:14 +0300

    HACK: clk: qcom: gcc-msm8953: make USB GDSC always on

--- drivers/clk/qcom/gcc-msm8953.c
+++ drivers/clk/qcom/gcc-msm8953.c
@@ -3852,6 +3852,11 @@ static struct gdsc usb30_gdsc = {
 		.name = "usb30",
 	},
 	.pwrsts = PWRSTS_OFF_ON,
+	.flags = ALWAYS_ON,
+	/*
+	 * FIXME: dwc3 usb gadget cannot resume after GDSC power off
+	 * dwc3 7000000.dwc3: failed to enable ep0out
+	 * */
 };
 
 static struct gdsc venus_gdsc = {
