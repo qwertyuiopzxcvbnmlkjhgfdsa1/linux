1d2748f75c7452c6f474139e0d2fb927af953ac8
Petr Blaha <petr.blaha@theifactory.com>
2022-03-10 13:25:04 +0100

    xiaomi daisy touchpad work 2?

--- drivers/input/touchscreen/goodix.c
+++ drivers/input/touchscreen/goodix.c
@@ -97,6 +97,7 @@ static const struct goodix_chip_id goodix_chip_ids[] = {
 	{ .id = "5663", .data = &gt1x_chip_data },
 	{ .id = "5688", .data = &gt1x_chip_data },
 	{ .id = "917S", .data = &gt1x_chip_data },
+	{ .id = "917D", .data = &gt1x_chip_data },
 	{ .id = "9286", .data = &gt1x_chip_data },
 
 	{ .id = "911", .data = &gt911_chip_data },
@@ -1396,6 +1397,7 @@ static const struct of_device_id goodix_of_match[] = {
 	{ .compatible = "goodix,gt912" },
 	{ .compatible = "goodix,gt9147" },
 	{ .compatible = "goodix,gt917s" },
+	{ .compatible = "goodix,gt917d" },
 	{ .compatible = "goodix,gt927" },
 	{ .compatible = "goodix,gt9271" },
 	{ .compatible = "goodix,gt928" },
